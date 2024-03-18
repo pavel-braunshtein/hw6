63ad257721e3d65eeef772b5817fef95a2a350df
pavelbraun333 <pavelbraun333@gmail.com>
2024-03-18 19:21:24 +0200

    project added

---
+++ hw_6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw_6
+//
+//  Created by Pavel on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
